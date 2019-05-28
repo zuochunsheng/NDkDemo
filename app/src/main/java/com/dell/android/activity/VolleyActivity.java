e79eddebb9b4dbc1fba3f49062d3cadebe6d53b2
zuochunsheng <zuochunsheng@ledcas.com>
2019-05-28 17:46:41 +0800

    下载ndk ,重新编译 2019-05-28

--- app/src/main/java/com/dell/android/activity/VolleyActivity.java
+++ app/src/main/java/com/dell/android/activity/VolleyActivity.java
@@ -1,38 +1,38 @@
-package com.dell.android.activity;
-
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.dell.android.R;
-
-public class VolleyActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_volley);
-
-
-        RequestQueue mQueue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest("http://www.baidu.com",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("TAG", response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("TAG", error.getMessage(), error);
-            }
-        });
-        mQueue.add(stringRequest);
-
-    }
-}
+//package com.dell.android.activity;
+//
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.util.Log;
+//
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.StringRequest;
+//import com.android.volley.toolbox.Volley;
+//import com.dell.android.R;
+//
+//public class VolleyActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_volley);
+//
+//
+//        RequestQueue mQueue = Volley.newRequestQueue(this);
+//        StringRequest stringRequest = new StringRequest("http://www.baidu.com",
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        Log.d("TAG", response);
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.e("TAG", error.getMessage(), error);
+//            }
+//        });
+//        mQueue.add(stringRequest);
+//
+//    }
+//}
