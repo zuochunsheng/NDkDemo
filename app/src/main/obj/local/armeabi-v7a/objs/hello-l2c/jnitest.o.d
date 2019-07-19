7b5d2dcbfd80a2753128caf42ced88747947b590
zuochunsheng <zuochunsheng@ledcas.com>
2019-07-19 18:56:52 +0800

    使用ndk-build 方式 生成 .so

---
+++ app/src/main/obj/local/armeabi-v7a/objs/hello-l2c/jnitest.o.d
@@ -0,0 +1,5 @@
+D:/android-test/TestNDk/app/obj/local/armeabi-v7a/objs/hello-l2c/jnitest.o: \
+  D:\android-test\TestNDk\app\jni\jnitest.c \
+  D:\android-test\TestNDk\app\jni\com_dell_android_activity_FirstActivity.h
+
+D:\android-test\TestNDk\app\jni\com_dell_android_activity_FirstActivity.h:
